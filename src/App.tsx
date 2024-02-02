56a0766de22b1e9866eba78129104c905305f980
koreaygj <ygj1828@gmail.com>
2024-02-02 16:56:08 +0900

    Feat : GI-11 react-router 구조
    로그인, 회원가입, 홈 페이지 router 설정 및 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./routes/login";
+import Register from "./routes/register";
+import Home from "./routes/home";
+import "./styles/style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+/*
+  route
+   - 로그인 : /login
+   - 회원가입 : /register
+   - 메인 : /
+   - 프로젝트 생성 : /project
+   - 메인 코드 에디터 : /project/code/{projectId}
+   - 채팅
+      - 프로젝트: /chat/project/{chatId}
+      - DM: /chat/dm/{memberId}
+   - 게시판
+      - 게시판 목록 : /board
+      - 게시글 보기 : /board/{boardId}
+      - 게시글 작성 : /board/new
+      - 게시글 수정 : /board/edit/{boardId}
+*/
+
+/* 
+ 리덕스 구조
+  store / state action 
+  
+  const counter = (state = initialState, action) => {
+    console.log(action);
+    switch(action.type) {
+      case INCREMENT:
+        return { 
+          number: state.number + action.diff
+        };
+      case DECREMENT:
+        return { 
+          number: state.number - 1
+        };
+      default:
+        return state;
+    }
+  }
+
+  // 스토어를 만들 땐 createStore 에 리듀서 함수를 넣어서 호출합니다.
+  const { createStore } = Redux;
+  const store = createStore(counter);
+
+
+*/
---
+++ src/routes/home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Sidebar from "../components/home/sidebar";
+const Home: React.FC = () => {
+  return <div>
+    <Sidebar/>
+  </div>;
+};
+export default Home;
