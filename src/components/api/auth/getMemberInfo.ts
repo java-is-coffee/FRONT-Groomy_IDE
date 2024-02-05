0ece494e15854e47164f99763bf333b1ca3c191e
koreaygj <ygj1828@gmail.com>
2024-02-05 23:20:25 +0900

    Feat : GI-47 get member info by api
    api로 부터 member 정보 가져오기
    그후 세션 스토리지에 저장

--- src/components/api/auth/getMemberInfo.ts
+++ src/components/api/auth/getMemberInfo.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { updateAccessToken } from "./updateAccessToken";
+import { Status } from "../status";
 
 const USER_API_URL =
   "http://ec2-54-180-2-103.ap-northeast-2.compute.amazonaws.com:8080/api/member";
@@ -7,11 +8,6 @@ const USER_API_URL =
 interface MemberDTO {
   status: Status;
   data: MemberInfo;
-}
-
-interface Status {
-  message: string;
-  code: number;
 }
 
 export interface MemberInfo {
@@ -27,15 +23,17 @@ export interface MemberInfo {
 export const getMemberInfo = async (): Promise<boolean> => {
   try {
     const accessToken = localStorage.getItem("accessToken");
-    const response = await axios.get<MemberDTO>(`${USER_API_URL}/my`, {
+    const response = await axios.get(`${USER_API_URL}/my`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
-    const status: Status = response.data.status;
     const memberInfo: MemberInfo = response.data.data;
-
-    if (status.code === 401) {
+    // 성공적으로 멤버 정보를 받아온 경우, sessionStorage에 저장
+    sessionStorage.setItem("member", JSON.stringify(memberInfo));
+    return true;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
       // AccessToken 갱신
       const updated = await updateAccessToken();
       if (updated) {
@@ -44,12 +42,6 @@ export const getMemberInfo = async (): Promise<boolean> => {
         return false;
       }
     }
-
-    // 성공적으로 멤버 정보를 받아온 경우, sessionStorage에 저장
-    sessionStorage.setItem("member", JSON.stringify(memberInfo));
-    return true;
-  } catch (error) {
-    console.error("서버 오류입니다.", error);
     return false;
   }
 };
