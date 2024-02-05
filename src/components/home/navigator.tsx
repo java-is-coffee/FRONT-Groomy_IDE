b9de72199d456b8fe465c64ea8043bdc84d366ba
koreaygj <ygj1828@gmail.com>
2024-02-05 18:42:21 +0900

    Feat : GI-47 새 프로젝트 navigator
    새 프로젝트 클릭시 navigator로 프로젝트 생성 창으로 이동

--- src/components/home/navigator.tsx
+++ src/components/home/navigator.tsx
@@ -3,6 +3,7 @@ import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { GoPlus } from "react-icons/go";
 
 import "../../styles/home/nav.css";
+import { useNavigate } from "react-router-dom";
 
 type navProps = {
   onChange: (newState: boolean) => void;
@@ -10,6 +11,7 @@ type navProps = {
 };
 
 const Nav: React.FC<navProps> = ({ onChange, sideClose }) => {
+  const navigate = useNavigate();
   return (
     <div className="menu-bar">
       <div
@@ -19,7 +21,10 @@ const Nav: React.FC<navProps> = ({ onChange, sideClose }) => {
         <MdKeyboardDoubleArrowRight size={"32px"} />
       </div>
       <div>
-        <button className="new-project">
+        <button
+          className="new-project"
+          onClick={() => navigate("/project/new")}
+        >
           <span>NEW PROJECT</span>
           <GoPlus size={"20px"} />
         </button>
