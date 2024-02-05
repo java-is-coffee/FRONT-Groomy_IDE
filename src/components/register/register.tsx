e18bcc653e9846f397a20c59f874633a33967476
gooowoon <147989051+gooowoon@users.noreply.github.com>
2024-02-05 23:42:03 +0900

    Refactor: 회원가입 페이지 수정

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -78,6 +78,16 @@ function Register() {
           </a>
         </div>
       </div>
+      <div className="already">
+        <div>
+          <span className="q">이미 계정이 있으세요?</span>
+        </div>
+        <div>
+          <a href="/login" className="login">
+            로그인
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
