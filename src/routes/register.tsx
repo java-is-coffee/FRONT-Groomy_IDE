7c4737731f1194b23a8ecf139de35302b8c2a3fa
Noh <nohsa97@nate.com>
2024-02-05 17:53:04 +0900

    Feat : GI-14 회원가입 기본 기능 추가
    - 회원 가입 관련된 기초 기능 추가.
      - 이메일, 비밀번호, 이름, 닉네임을 입력받고 이를 API 통신하는 기능
      - 이메일이 중복될 시, 중복되었다고 alert,
      - 비밀번호 2번째 체크에서 일치하지 않을 시, 다시 입력하도록 틀렸다고 alert하고 해당 칸 비우기
    - registr라우터 수정
    - registercss 는 기본 베이스만 수정 (고운님 수정 완료 시, 적용 예정)

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import LoginComponent from "../components/login/login";
+import Register from "../components/register/register";
 
-function login() {
+function register() {
   return (
     <div>
-      <LoginComponent />
+      <Register />
     </div>
   );
 }
 
-export default login;
+export default register;
+
+/* 
+이메일
+비밀번호 (영문, 숫자, 특수문자 8 ~ 30)
+비밀번호 확인
+이름 (2 ~ 30)
+닉네임 (2 ~ 30)
+
+회원가입 (버튼)
+이미 계정이 있으세요?  로그인(링크) 
+
+*/
