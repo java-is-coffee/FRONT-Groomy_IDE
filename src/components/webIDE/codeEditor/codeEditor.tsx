5bb0dcb9ae906bceff0c6f4eea4db28a30601b91
koreaygj <ygj1828@gmail.com>
2024-02-12 19:57:13 +0900

    Feat : GI-15 코드 에디터 저장 추가
    cmd + s로 저장 구현

--- src/components/webIDE/codeEditor/codeEditor.tsx
+++ src/components/webIDE/codeEditor/codeEditor.tsx
@@ -1,14 +1,21 @@
 import { Editor, useMonaco } from "@monaco-editor/react";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import CodeTab from "./codeTab";
 
 import "../../../styles/webIDE/codeContainer.css";
-import { editor } from "monaco-editor";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../redux/store/store";
+import * as monaco from "monaco-editor";
+import {
+  FileItem,
+  saveItem,
+} from "../../../redux/reducers/ide/fileSystemReducer";
+import {
+  CodeDetails,
+  saveCode,
+} from "../../../redux/reducers/ide/editingCodeReducer";
 
-interface EditorState {
-  code: string;
-}
-
+// theme enum
 enum EditorTheme {
   iplastic = "iplastic",
   slush = "slushAndProppies",
@@ -17,25 +24,86 @@ enum EditorTheme {
 }
 
 const CodeEditor = () => {
-  const [theme, setTheme] = useState<EditorTheme>(EditorTheme.xcode);
-  useEffect(() => {}, []);
-  const monaco = useMonaco();
+  const theme = EditorTheme.iplastic;
+  // 테마 변경 기능시 사용할 state
+  //const [theme, setTheme] = useState<EditorTheme>(EditorTheme.xcode);
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
 
+  // redux에서 현재 edit중인 코드 정보 가져오기
+  const curFile = useSelector(
+    (state: RootState) => state.editingCode.editingCode
+  );
+  const monacoInstance = useMonaco();
+  const dispatch = useDispatch();
+
+  // 저장 메서드
+  const saveDocument = useCallback(() => {
+    const value = editorRef.current!.getValue();
+    const item: FileItem = {
+      id: curFile.id,
+      name: curFile.name,
+      type: "file",
+      content: value,
+    };
+    dispatch(saveItem(item));
+
+    const code: CodeDetails = {
+      id: curFile.id,
+      name: curFile.name,
+      lang: curFile.name.split(".")[1],
+      content: value,
+    };
+    dispatch(saveCode(code));
+
+    // 코드 저장 api 추가 예정
+  }, [curFile, dispatch]);
+
+  // cmd + c || ctrl + c 입력시 저장 메서드 호출
   useEffect(() => {
-    if (monaco) {
+    if (editorRef.current && curFile) {
+      const disposable = editorRef.current.onKeyDown((e) => {
+        if ((e.metaKey || e.ctrlKey) && e.keyCode === monaco.KeyCode.KeyS) {
+          e.preventDefault();
+          saveDocument();
+        }
+      });
+
+      return () => disposable.dispose();
+    }
+  }, [curFile, dispatch, saveDocument]);
+
+  // monaco editor 저장시 ref 세팅
+  const handleEditorDidMount = (
+    editor: monaco.editor.IStandaloneCodeEditor,
+    monaco: any
+  ) => {
+    editorRef.current = editor;
+  };
+
+  // const handleTheme = () => {
+  //   setTheme(EditorTheme.xcode);
+  // };
+
+  // monaco code editor theme setting
+  useEffect(() => {
+    if (monacoInstance) {
       // 테마 정의
       fetch(`./theme/ide/${theme}.json`)
         .then((data) => data.json())
         .then((themeData) => {
-          monaco.editor.defineTheme(`${theme}`, themeData);
-          monaco.editor.setTheme(`${theme}`);
+          monacoInstance.editor.defineTheme(`${theme}`, themeData);
+          monacoInstance.editor.setTheme(`${theme}`);
         });
     }
-  }, [monaco]);
+  }, [monacoInstance, theme]);
   return (
     <div className="code-container">
       <CodeTab />
-      <Editor language="java" value="" />
+      <Editor
+        language={curFile.lang}
+        value={curFile.content}
+        onMount={handleEditorDidMount}
+      />
     </div>
   );
 };
