046eea0d96bcfbed033c95a251be799f0253669c
koreaygj <ygj1828@gmail.com>
2024-02-16 10:29:29 +0900

    Feat : GI-19 프로젝트 초대 받아오기
    프로젝트 참여 요청 받는 api

---
+++ src/api/project/getInvitedProjectList.ts
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { patchAccessToken } from "../auth/patchAccessToken";
+import { ProjectDetails } from "./patchProjectList";
+
+const USER_API_URL =
+  "http://ec2-54-180-2-103.ap-northeast-2.compute.amazonaws.com:8080/api/ide";
+
+// projectList 가져오는 메서드
+export const getInvitedProjects = async (): Promise<
+  ProjectDetails[] | null
+> => {
+  console.log("projectList fetching...");
+  const storedToken = localStorage.getItem("accessToken");
+  if (!storedToken) {
+    console.error("Access token not found. Please login again.");
+    return null;
+  }
+  const config = {
+    headers: {
+      Authorization: `Bearer ${storedToken}`,
+    },
+  };
+  try {
+    const response = await axios.get<ProjectDetails[]>(
+      `${USER_API_URL}/invited-list`,
+      config
+    );
+    console.log(response.data);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.error(
+        "Failed to fetch project list with status code:",
+        response.status
+      );
+      return null;
+    }
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Error fetching project list:", error.response?.data);
+      if (error.response?.status === 401) {
+        const isTokenRefreshed = await patchAccessToken();
+        if (isTokenRefreshed) {
+          return getInvitedProjects();
+        }
+      }
+    } else {
+      console.error(
+        "An unexpected error occurred while fetching project list:",
+        error
+      );
+    }
+
+    return null;
+  }
+};
