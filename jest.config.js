61f56e23c63e6877d8883ed85b2a6057813a2ed8
Yang Gyeong Jin <62227770+koreaygj@users.noreply.github.com>
2024-02-20 22:22:15 +0900

    Fix : GI-17 jest 설정 변경
    react-mackdown-preview 테스트 제외

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,9 @@ module.exports = {
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
-  transformIgnorePatterns: ["node_modules/(?!axios)/"],
+  transformIgnorePatterns: [
+    "node_modules/(?!axios|react-markdown|@uiw/react-markdown-preview)/"
+  ],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "^monaco-editor": "<rootDir>/node_modules/monaco-editor",
