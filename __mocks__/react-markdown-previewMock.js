7ebe119ac1130c4a5c1f50b19635542583e1524b
Yang Gyeong Jin <62227770+koreaygj@users.noreply.github.com>
2024-02-20 23:03:40 +0900

    Feat : GI-17 test mocks js

---
+++ __mocks__/react-markdown-previewMock.js
@@ -0,0 +1,6 @@
+// __mocks__/react-markdown-previewMock.js
+module.exports = {
+  // 여기에 필요한 모의 함수나 객체를 구현합니다.
+  // 예를 들어, 라이브러리에서 제공하는 컴포넌트나 함수를 모의하려면 아래와 같이 작성할 수 있습니다.
+  MarkdownPreview: () => 'Mocked Markdown Preview',
+};
