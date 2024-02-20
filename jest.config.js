d18e1af9e0e4a22b980afa3a24caf2e05f683ee5
Yang Gyeong Jin <62227770+koreaygj@users.noreply.github.com>
2024-02-20 23:21:06 +0900

    Fix : GI-17 esMoudle add

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,28 @@
+const esModules = [ 
+  "react-markdown",
+  "vfile",
+  "unist-.+",
+  "unified",
+  "bail",
+  "is-plain-obj",
+  "trough",
+  "remark-.+",
+  "mdast-util-.+",
+  "micromark",
+  "parse-entities",
+  "character-entities",
+  "property-information",
+  "comma-separated-tokens",
+  "hast-util-whitespace",
+  "remark-.+",
+  "space-separated-tokens",
+  "decode-named-character-reference",
+  "ccount",
+  "escape-string-regexp",
+  "markdown-table",
+  "trim-lines",
+].join("|"); 
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
@@ -5,11 +30,12 @@ module.exports = {
     "^.+\\.(ts|tsx|js|jsx)$": "babel-jest", // JS와 JSX 파일도 변환하도록 설정을 추가합니다.
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(@uiw/react-markdown-preview|@uiw/react-md-editor|react-markdown)/)", // 변환해야 하는 모듈을 포함시킵니다.
+    "node_modules/(?!(@uiw/react-markdown-preview|@uiw/react-md-editor|react-markdown)/)",
+    [`(${esModules}).+\\.js$`]: '<rootDir>/config/jest/babelTransform.js', 
   ],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "^monaco-editor": "<rootDir>/node_modules/monaco-editor",
-    "^@uiw/react-markdown-preview$": "<rootDir>/__mocks__/react-markdown-previewMock.js",
+    `[/\\\\]node_modules[/\\\\](?!${esModules}).+\\.(js|jsx|mjs|cjs|ts|tsx)$`,
   },
 };
