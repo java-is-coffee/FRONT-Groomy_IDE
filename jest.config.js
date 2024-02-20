ae1d8c0c05ae4ddc9f8416af369b5358473e0771
Yang Gyeong Jin <62227770+koreaygj@users.noreply.github.com>
2024-02-20 22:39:30 +0900

    Fix : GI-17 test error
    js, jsx 추가

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx|js|jsx)$": "ts-jest", // JS와 JSX 파일도 변환하도록 설정을 추가합니다.
   },
   transformIgnorePatterns: [
-    "node_modules/(?!axios|react-markdown|@uiw/react-markdown-preview)/"
+    "node_modules/(?!(@uiw/react-markdown-preview|@uiw/react-md-editor|react-markdown)/)", // 변환해야 하는 모듈을 포함시킵니다.
   ],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
