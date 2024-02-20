c4ce8a139c31e1e2975c741323445e4b4f119b81
koreaygj <ygj1828@gmail.com>
2024-02-21 00:59:02 +0900

    Remove : GI-35 test file

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,8 @@
-const esModules = [ 
-  "react-markdown",
-  "vfile",
-  "unist-.+",
-  "unified",
-  "bail",
-  "is-plain-obj",
-  "trough",
-  "remark-.+",
-  "mdast-util-.+",
-  "micromark",
-  "parse-entities",
-  "character-entities",
-  "property-information",
-  "comma-separated-tokens",
-  "hast-util-whitespace",
-  "remark-.+",
-  "space-separated-tokens",
-  "decode-named-character-reference",
-  "ccount",
-  "escape-string-regexp",
-  "markdown-table",
-  "trim-lines",
-].join("|"); 
-
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.(ts|tsx|js|jsx)$": "babel-jest", // JS와 JSX 파일도 변환하도록 설정
-    [`(${esModules}).+\\.js$`]: "babel-jest", // esModules에 해당하는 파일도 변환
   },
   transformIgnorePatterns: [
     `node_modules/(?!(${esModules}))/`, // esModules에 해당하지 않는 모듈은 변환에서 제외
