20d5db84e164fa8fcc1973bc8b56b55573361f47
Yang Gyeong Jin <62227770+koreaygj@users.noreply.github.com>
2024-02-20 23:30:34 +0900

    Fix : GI-17 jest file update

--- jest.config.js
+++ jest.config.js
@@ -27,15 +27,15 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest", // JS와 JSX 파일도 변환하도록 설정을 추가합니다.
-    [`(${esModules}).+\\.js$`]: '<rootDir>/config/jest/babelTransform.js', 
+    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest", // JS와 JSX 파일도 변환하도록 설정
+    [`(${esModules}).+\\.js$`]: "babel-jest", // esModules에 해당하는 파일도 변환
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(@uiw/react-markdown-preview|@uiw/react-md-editor|react-markdown)/)",
-    `[/\\\\]node_modules[/\\\\](?!${esModules}).+\\.(js|jsx|mjs|cjs|ts|tsx)$`,
+    `node_modules/(?!(${esModules}))/`, // esModules에 해당하지 않는 모듈은 변환에서 제외
   ],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "^monaco-editor": "<rootDir>/node_modules/monaco-editor",
+    // 필요한 경우 추가 모의 객체 경로 설정
   },
 };
